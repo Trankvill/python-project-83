45713b0cd3b5cfcd49368b591a0c3a49c906d5f5
Konstantin Chupin <chupin.k@yandex.ru>
2023-02-05 17:47:40 +0500

    fix db.py

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -29,6 +29,30 @@ def get_queries_for_urls():
     return site
 
 
+def get_queries_for_create_url_exist(dt, form):
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute('SELECT id FROM urls WHERE name=(%s);',
+                (form['url'],))
+    id_find = cur.fetchone()
+    conn.commit()
+    cur.close()
+    return id_find
+
+
+def get_queries_for_create_url_not_exist(dt, form):
+    conn = connect_to_db()
+    cur = conn.cursor()
+    cur.execute("INSERT INTO urls (name, created_at) VALUES (%s, %s);",
+                (form['url'], dt))
+    cur.execute('SELECT id FROM urls WHERE name=(%s);',
+                (form['url'],))
+    id_find = cur.fetchone()
+    conn.commit()
+    cur.close()
+    return id_find
+
+
 def get_queries_for_show_url(id):
     conn = connect_to_db()
     cur = conn.cursor()
@@ -50,6 +74,8 @@ def get_queries_for_site_check(id):
     cur.execute('SELECT name FROM urls WHERE id=(%s);',
                 (id,))
     site = cur.fetchone()
+    conn.commit()
+    cur.close()
     return site
 
 
